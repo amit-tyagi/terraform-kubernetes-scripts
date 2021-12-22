cfd8e27d78e604ce0f65f28d2b571a797906df41
Tyagi, Amit <iamittyagi@gmail.com>
2021-12-22 08:47:52 -0600

    Few corrections

--- azure/main.tf
+++ azure/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
       name                              = "system"
       node_count                        = var.system_node_count
       vm_size                           = "Standard_DS2_v2"
-      type                              = "VirtualMachinesScaleSets"
+      type                              = "VirtualMachineScaleSets"
       availability_zones                = [1, 2, 3]
       enable_auto_scaling               = false
     }
--- azure/providers.tf
+++ azure/providers.tf
@@ -6,7 +6,9 @@ terraform {
   required_providers {
       azurerm = {
           source = "hashicorp/azurerm"
-          version = "2.39.0"
+          version = "2.66.0"
       }
   }
+
+  required_version = ">= 0.14"
 }
