f4489c1a36b3066cd0c6ae327534f4bb4dd679b0
Tyagi, Amit <iamittyagi@gmail.com>
2021-12-23 10:17:39 -0600

    More changes for azure

--- azure/main.tf
+++ azure/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
       vm_size                           = "Standard_DS2_v2"
       type                              = "VirtualMachineScaleSets"
       availability_zones                = [1, 2, 3]
-      enable_auto_scaling               = false
+      enable_auto_scaling               = true
     }
 
     identity {
